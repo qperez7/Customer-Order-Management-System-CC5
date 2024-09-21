ef8ea03519944d5d4e61116f43bba6341d7920ea
Quirianyailet Perez <qperez1@usf.edu>
2024-09-20 22:09:35 -0400

    Create checkPendingOrders function

--- order_management_system.js
+++ order_management_system.js
@@ -14,30 +14,45 @@ console.log(orders);// Output: An empty array of orders
 
 // Task 3: Create a Function to Place an Order
 var itemsOrdered = [
-    {name: "Expresso",price:3,quantity:1},
-    {name: 'Latte', price: 4, quantity: 3 },
+    {name: "Expresso",price:3,quantity:3},
 ]
 
 function placeOrder(customerName,itemsOrdered) {
-  if (itemsOrdered.forEach( item => {inventory.name}));
-        return {
-            customerName,
-            itemsOrdered,
-            status: "Pending",
+if(itemsOrdered.some(item => inventory.name === itemsOrdered.name))
+    return {
+        customerName,
+        itemsOrdered,
+        status: "Pending"
         }
-}
-console.log(orders.push(placeOrder));
+else 
+return {itemsOrdered:"error item not found"}
+}   
+let updateitemsOrdered =  inventory[0].quantity - itemsOrdered[0].quantity
+inventory.push(updateitemsOrdered)
+console.log(orders.push(placeOrder("Alice",itemsOrdered))); // Output: places order in order array
 
 //Task 4: Create a Function to Calculate Total for an Order
-//let totalprice = itemsOrdered.price
-//let totalOrdered = totalprice.reduce((acc,currentvalue) => acc + currentvalue,0);
-//console.log(totalOrdered);
+function calculateOrderTotal() {
+let total = itemsOrdered.reduce((i,price) => i + price,0)
+return total;
+}
+var itemsOrdered = [3,3,3]
+ 
+console.log(calculateOrderTotal(`total price: $${itemsOrdered}`)); // Output: Calculates totals for items ordered
+
 
 //Task 5: Create a Function to Mark an Order as Completed
 function completeOrder(Name) {
-  if(  orders.find((customerName) => customerName === Name))
+  if(orders.find( order => order.customerName === Name))
     return {Name,status: "Completed"}
   else
   return {customerName:"Order is not found"}
 };
-console.log(completeOrder("Alice"));
+console.log(completeOrder('Alice'));// Output: Finds orders and marks them complete
+
+//Task 6: Create a Function to Check Pending Orders
+function checkPendingOrders(orders) {
+    orders.forEach(status => { if(status === "Pending")
+    return customerName,itemsOrdered,status
+})};
+console.log(checkPendingOrders(orders)); // Output: is undefined there are no pending orders
